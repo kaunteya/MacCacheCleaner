686b30366d4a43aed4d81c55b7565d50906dbb8e
Kaunteya <>
2019-07-30 15:47:36 +0530

    Minor bug fix

--- MacCacheCleaner/MainViewController.swift
+++ MacCacheCleaner/MainViewController.swift
@@ -30,6 +30,8 @@ class MainViewController: NSViewController, StoryboardLoadable {
 extension MainViewController: CacheTableViewDelegate {
     func cacheListUpdateStatusChanged(status: CacheList.UpdateStatus) {
         Log.info("Update status \(status)")
-        loadingView.isHidden = status == .completed
+        DispatchQueue.main.async { [weak self] in
+            self?.loadingView.isHidden = status == .completed
+        }
     }
 }
